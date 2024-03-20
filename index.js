eaad716bca433347ec28a266aa7da82fe14888c6
Flick23 <vendas.chaves23@gmail.com>
2024-03-20 18:33:37 -0300

    treinando

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
-console.log("oi")
+/// adicionando variaveis
+let nomeDoheroi = "Flick"
+let nivelDoheroi = "20"
+console.log ( nomeDoheroi +    " está no nivel " + 20 +  " de sua jornada inicial " )
+
 
